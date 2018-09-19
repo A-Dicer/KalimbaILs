2cd56b49f5036c05c720131c96d09e615a66b632
Andrew Dicer <andrew.dicer@gmail.com>
2018-09-19 07:23:52 -0500

    first build

--- config/twitchAuth.js
+++ config/twitchAuth.js
@@ -2,7 +2,7 @@ module.exports = {
     'twitchAuth' : {
         'clientID'      : 'b6n9ati9hgbm1mkg4t5kkdyiktyzbe', // your App ID
         'clientSecret'  : 'cg6mo725gfzfubqb9lee9982sqcvou', // your App Secret
-        'callbackURL'   : 'http://kalimbril.herokuapp.com/api/auth/twitch/callback',
+        'callbackURL'   : 'https://kalimbril.herokuapp.com/api/auth/twitch/callback',
         'scope'         : 'user_read'
     },
 };
